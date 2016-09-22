a4d6669fafb25decc6ac4206cb3381017e5620ff
linciping <linciping@outlook.com>
2016-09-22 21:34:21 +0800

    dagger注入和缓存工具

--- app/src/main/java/com/example/administrator/dagger/dagger/component/ActivityComponent.java
+++ app/src/main/java/com/example/administrator/dagger/dagger/component/ActivityComponent.java
@@ -1,17 +1,19 @@
 package com.example.administrator.dagger.dagger.component;
 
+import android.app.Activity;
+
 import com.example.administrator.dagger.dagger.module.ActivityModule;
 import com.example.administrator.dagger.dagger.scope.ActivityScope;
 
-import javax.inject.Singleton;
-
 import dagger.Component;
-import dagger.Module;
 
 /**
  * Created by Administrator on 2016/9/17.
  */
 @ActivityScope
 @Component(dependencies = AppComponent.class,modules = ActivityModule.class)
-public class ActivityComponent {
+public interface ActivityComponent {
+
+    Activity activity();
+    void inject(Activity activity);
 }
