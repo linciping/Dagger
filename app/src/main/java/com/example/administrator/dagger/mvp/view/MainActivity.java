df4c979315467e0e3b40095e3deb131547aaac5f
linciping <linciping@outlook.com>
2016-09-22 21:48:42 +0800

    Revert "Revert "Revert "dagger注入和缓存工具"""
    This reverts commit e4f67dfb5ced92877dbd374da15667d17c71f0a1.

--- app/src/main/java/com/example/administrator/dagger/mvp/view/MainActivity.java
+++ app/src/main/java/com/example/administrator/dagger/mvp/view/MainActivity.java
@@ -1,48 +1,21 @@
 package com.example.administrator.dagger.mvp.view;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import com.example.administrator.dagger.BaseActivity;
 import com.example.administrator.dagger.R;
-import com.example.administrator.dagger.SpfManager;
+import com.example.administrator.dagger.http.ApiService;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Inject
-    SpfManager spfManager;
-
-    @BindView(R.id.btn_cache)
-    Button btnCache;
-    @BindView(R.id.ext_ipaddress)
-    EditText extIpAddress;
-    @BindView(R.id.btn_send)
-    Button btnSend;
-    @BindView(R.id.txt_county)
-    TextView txtCounty;
-    @BindView(R.id.txt_city)
-    TextView txtCity;
+    ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        getActivityComponent().inject(this);
-        btnCache.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                spfManager.put("cache","cache");
-            }
-        });
     }
 }
